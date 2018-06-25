659f8ba0cf9514d3b0ac326836368fb6908db2ed
revsic <revsic99@gmail.com>
2018-06-26 02:03:06 +0900

    Update Preprocess - Add method create_dataset

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
+import csv
 import numpy as np
 
-SEOUL_MAP = [ # 9 by 8 matrix, 25 borough
+# 9 x 8 matrix, 25 city
+SEOUL_MAP = [
     [0, 0, 0, 0, '도봉구', '노원구', 0, 0],
     [0, 0, 0, '강북구', '강북구', '노원구', '노원구', 0],
     [0, '은평구', '종로구', '성북구', '성북구', '성북구', '중랑구', 0],
@@ -12,107 +14,124 @@ SEOUL_MAP = [ # 9 by 8 matrix, 25 borough
     [0, '구로구', '금천구', '관악구', '서초구', 0, 0, 0]
 ]
 
-def read_csv(name, location_cnt=39):
-    """Read the csv file and return the amount of fine dust for each region grouped by date.
+
+def read_csv(name, num_region=39):
+    """Read the csv file and return the pm10 data
     
     Args:
-        name: The name of the csv file to read.
-    
+        name: str, the name of the csv file.
+        num_region: int, the number of the region including city and road.
+
     Returns:
-        Dictionary object mapping the amount of fine dust for each region by date.
+        dist, dust data grouped by date and city.
         {'20170506': {'강남구': 50, '강동구': 60, '강서구':70}, '20170507': {'강남구': } ...}
     
     Raises:
-        ValueError: If the number of the data per day in the csv file is not equal to LOCATION_CNT.
-        
+        ValueError: If the number of region in the csv file is not equal to `num_region`.
     """
-    with open(name) as f:
-        raw_data = f.read().strip()
+    with open(name, encoding='utf-8') as f:
+        raw = csv.reader(f)
+        raw.__next__()
 
-    del_quote = raw_data.replace("\"", '')
-    data = list(map(lambda x: x.split(','), del_quote.split('\n')))[1:] # [1:] csv header
+        date = ''
+        group = []
+        group_by_date = []
 
-    splitted = []
+        for i, line in enumerate(raw):
+            if i % num_region == 0:
+                group_by_date.append([])
+                group = group_by_date[-1]
 
-    ptr = 0
-    for i in range(len(data) // location_cnt):
-        splitted.append(data[ptr:ptr+location_cnt])
-        ptr += location_cnt
-    
-    ## test case
-    for date_list in splitted:
-        date = date_list[0][0] # index 0:date
-        for local in date_list:
-            if date != local[0]:
-                raise ValueError(date + ' is not same as ' + 'local[0]')
-    
-    def filter_borough(dic):
-        return dict(filter(lambda t: '구' in t[0], dic.items())) #filter not road name only borough 
+                date = line[0]
 
-    # index 0:date, 1:local name, 6:pms
-    pms = dict(map(lambda x: (x[0][0], dict(map(lambda t: (t[1], t[6]), x))), splitted))
-    pms_filtered = dict(filter(lambda x: '' not in x[1].values(), pms.items())) # csv data contains spaces
-    pms_filtered2 = dict(map(lambda x: (x[0], filter_borough(x[1])), pms_filtered.items()))
-    
-    return pms_filtered2
+            if date == line[0]:
+                group.append(line)
+            else:
+                raise ValueError(date + 'is not same as ' + line[0])
 
-def geographical_mapping(pms_data):
-    """Map the amount of fine dust for each region to geographical map of Seoul.
+    group_by_city = {}
+    for regions in group_by_date:
+        date = regions[0][0]
+        group_by_city[date] = {}
+
+        group = group_by_city[date]
+        for region in regions:
+            date, name, no2, o3, co, so2, pm10, pm20 = region
+            if name.endswith('구'):
+                if pm10 != '':
+                    group[name] = int(pm10)
+                else:
+                    del group_by_city[date]
+                    break
+
+    return group_by_city
+
+
+def geographical_mapping(dust_data, seoul_map=SEOUL_MAP):
+    """Map dust data to geographical map of Seoul.
     
     Args:
-        pms_data: Fine dust data pre-processed by read_csv.
-            Dictionary obejct mapping the amount of fine dust for each region by date.
-            {'20170506': {'강남구': 50, '강동구': 60, '강서구':70}, '20170507': {'강남구': } ...}
+        dust_data: dict, dust data grouped by date and city (from `read_csv`).
+            {'20170506': {'강남구': 50, '강동구': 60, '강서구': 70}, '20170507': {'강남구': } ...}
+        seoul_map: list of int, geographical map of Seoul.
     
     Returns:
-        Dictionary that map the amount of fine dust to the geographical map of Seoul.
+        dict, dust data mapped by the geographical map of Seoul, `seoul_map`.
         {'20170506': [[0, 0, 0, 0, 50, 60, 0, 0], [0, 0, 0, 40, ...]]}
       
     """
-    def dict2seoul(p):
-        return list(map(lambda t: list(map(lambda x: int(p[x]) if x != 0 else 0, t)), seoul_map))
+    mapped = {}
+    for date, data in dust_data.items():
+        mapped[date] = []
+        map_of_seoul = mapped[date]
 
-    # map dict to seoul geographic map
-    pms_mapped = dict(map(lambda p: (p[0], dict2seoul(p[1])), pms_data.items())) 
-    return pms_mapped
+        for line in seoul_map:
+            transformed = list(map(lambda x: data[x] if x != 0 else 0, line))
+            map_of_seoul.append(transformed)
 
-def generate_dataset(data, weekly_batch=7):
-    """ Generate the daily average amount of the fine dust(pm10) bundled in 7 days.
+    return mapped
+
+
+def generate_dataset(data, len_time=7):
+    """ Generate the dust dataset grouped by N days.
     
     Args:
-        data: Fine dust data pre-processed by read_csv
-            Dictionary object mapping the amount of fine dust for each region by date.
-            {'20170506': {'강남구': 50, '강동구': 60, '강서구':70}, '20170507': {'강남구': } ...}
-    
+        data: dict, dust data grouped by date and city (from `read_csv`)
+            {'20170506': {'강남구': 50, '강동구': 60, '강서구': 70}, '20170507': {'강남구': } ...}
+        len_time: int, length of the time step. dataset is grouped by `len_time` days.
+
     Returns:
-        pms_sampled: the amount of fine dust bundled in 7 days.
-        pms_result: the amount of fine dust on the next 7 days.
-        
+        sampled: the amount of fine dust bundled in 7 days.
+        result: the amount of fine dust on the next 7 days.
     """
-    pms_mapped = geographical_mapping(data)
+    mapped = geographical_mapping(data)
     
     # tie data to WEEKLY_BATCH(7) batches
-    pms_data = list(map(lambda x: x[1], sorted(pms_mapped.items()))) 
-    pms_tied = list(map(lambda i: pms_data[i:i+weekly_batch], range(len(pms_data) - weekly_batch)))
-    
-    pms_sampled = pms_tied[:-1]
-    pms_result = pms_tied[1:]
-    
-    return pms_sampled, pms_result
+    data = list(map(lambda x: x[1], sorted(mapped.items())))
+    tied = list(map(lambda i: data[i:i+len_time], range(len(data) - len_time)))
 
-def main():
-    csv_name = 'MonthlyAverageAirPollutionInSeoul.csv'
-    pms_data = read_csv(csv_name)
-    pms_sampled, pms_result = generate_dataset(pms_data)
+    sampled = tied[:-1]
+    result = tied[1:]
+    
+    return sampled, result
 
-    data_set = list(zip(pms_sampled, pms_result))
+
+def create_dataset(csv_name='./MonthlyAverageAirPollutionInSeoul.csv',
+                   len_time=7,
+                   train_rate=0.7):
+    data = read_csv(csv_name)
+    sampled, result = generate_dataset(data, len_time)
+
+    data_set = list(zip(sampled, result))
     np.random.shuffle(data_set)
 
-    train_set = data_set[:TRAIN_SIZE]
+    train_size = int(len(data_set) * train_rate)
+    train_set = data_set[:train_size]
     train_sampled = list(map(lambda x: x[0], train_set))
     train_result = list(map(lambda x: x[1], train_set))
 
-    test_set = data_set[-TEST_SIZE:]
+    test_set = data_set[-train_size:]
     test_sampled = list(map(lambda x: x[0], test_set))
     test_result = list(map(lambda x: x[1], test_set))
 
+    return (train_sampled, train_result), (test_sampled, test_result)
